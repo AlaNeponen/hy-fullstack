8586ebabefad4fd4df1d15dad7d1ec16403cd3c9
Teemu Närhi <t.a.narhi@gmail.com>
2020-11-20 17:13:06 +0200

    tehtävä 1.7

---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h1>Statistics</h1>
+      <Statistics text="good" amount={good} />
+      <Statistics text="neutral" amount={neutral} />
+      <Statistics text ="bad" amount={bad} />
+    </div>
+  )
+}
+const Statistics = (props) => {
+  return (
+    <div>
+      <p>{props.text} {props.amount}</p>
+    </div>
+  )
+}
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
+
