6e0794e1becb860f0f856f4344149a0e35f323e8
Teemu Närhi <t.a.narhi@gmail.com>
2021-01-22 23:31:56 +0200

    4.4

--- osa4/tests/list_helper.test.js
+++ osa4/tests/list_helper.test.js
@@ -1,9 +1,60 @@
-const { TestScheduler } = require('jest')
 const listHelper = require('../utils/list_helper')
+
 
 test('dummy returns one', () => {
     const blogs = []
 
     const result = listHelper.dummy(blogs)
     expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  const listWithOne = [
+    {
+      title: 'eka',
+      author: 'meikä',
+      url: 'notarealurl.org',
+      likes: 4
+    }
+  ]
+
+  const listWithMultiple = [
+      {
+        title: 'eka',
+        author: 'meikä',
+        url: 'notarealurl.org',
+        likes: 4
+      },
+
+      {
+          title: 'toka',
+          author: 'joku muu',
+          url: 'stillnotreal.fi',
+          likes: 1
+      },
+
+      {
+          title: 'kolmas',
+          author: 'sama äijä',
+          url: 'eivielakaanoikeaurl.com',
+          likes: 5
+      }
+  ]
+  const empty = []
+
+  test('empty list has 0 total likes', () => {
+      const result = listHelper.totalLikes(empty)
+      expect(result).toBe(0)
+  })
+
+  test('total likes of list with one blog equal that of the blog', () => {
+      const result = listHelper.totalLikes(listWithOne)
+      expect(result).toBe(4)
+  })
+
+  test('total likes are calculated properly for lists with multiple blogs', () => {
+      const result = listHelper.totalLikes(listWithMultiple)
+      expect(result).toBe(10)
+  })
+
 })
--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,9 +1,15 @@
-const Blog = require('../models/blog')
-
 const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    let total = 0
+    blogs.forEach(blogi=> {
+        total += blogi.likes  
+    });
+    return total
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
